2f5a2b9e841eb5f9fbe407548f2589a5a5d11f8f
Sri Ram <sriramrajsatapathi2004@gmail.com>
2026-03-06 23:36:49 +0530

    Added a Math problem

---
+++ Math/IsPrime.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class IsPrime {
+    public static boolean isPrime(int n){
+        if(n < 1) return false;
+        for(int i=2;i<Math.sqrt(n);i++){
+            if(n % i == 0){
+                return false; 
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number :");
+        int n = sc.nextInt();
+        boolean result = isPrime(n);
+        if(result == true){
+            System.out.println("It is prime");
+        }
+        else{
+            System.out.println("It is not prime");
+        }
+
+    }
+}
